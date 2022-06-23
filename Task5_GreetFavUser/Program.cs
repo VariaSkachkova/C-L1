d05310d64744664e5d926e3aa472cd4af073bc91
Varia S <especificita@gmail.com>
2022-06-23 16:40:57 +0300

    Greet favourite user - if... else

---
+++ Task5_GreetFavUser/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Hey Masha!");
+}
+else
+{
+    Console.WriteLine ("Hey ");
+    Console.WriteLine (username);
+}
